f642eb05e57951c2e48ae727b8508fa27b824549
Steven Bassett <steven.j.bassett@gmail.com>
2014-06-01 12:19:34 -0700

    Step 1, base project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * main JS file for awesome theme
+ *
+ */
+(function($){
+  var Drupal = window.Drupal || {behaviours: function(settings){}};
+
+  Drupal.behaviours.attach( settings ){
+
+  };
+})(jQuery)
