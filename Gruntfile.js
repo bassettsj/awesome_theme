45be8d5071e36cf815ae74ed365491e2b71a2fc4
Steven Bassett <steven.j.bassett@gmail.com>
2014-06-01 12:47:39 -0700

    configured the sass task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,28 @@
-module.exports = function(grunt){};
+module.exports = function(grunt){
+  grunt.initConfig({
+    package: grunt.file.readJSON('package.json'),
+    // task
+    sass: {
+      // target
+      theme:{
+        options:{
+          // note* requires SASS >=3.3.0 `gem install sass --pre`
+          sourcemap: true
+        },
+        // files
+        files: [{
+          dest: 'dist/css/awesome-theme.css',
+          src: 'scss/main.scss'
+        }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask(
+    'default',
+    'Compile SASS',
+    ['sass:theme']
+  );
+};
