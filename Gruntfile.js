33dae8697394b7427b6badc330df5149abfa31f7
Steven Bassett <steven.j.bassett@gmail.com>
2014-06-02 13:34:36 -0500

    Adding the cssmin task for the example theme

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,23 @@
 module.exports = function(grunt) {
+
   grunt.initConfig({
     package: grunt.file.readJSON('package.json'),
+    banner:
+        "/*! <%= package.title || package.name %> - v<%= package.version %> - "
+      + "<%= grunt.template.today(\"yyyy-mm-dd\") %>\n"
+      + "* Copyright (c) <%= grunt.template.today(\"yyyy\") %> <%= package.author.name %>;"
+      + " Licensed <%= package.licenses %> */\n",
     // task
     sass: {
       // target
       theme: {
         options: {
           // note* requires SASS >=3.3.0 `gem install sass --pre`
-          sourcemap: true
+          sourcemap: true,
+          loadPath: [
+            'bower_components',
+            'bower_components/bootstrap-sass-official/vendor/assets/stylesheets'
+          ]
         },
         // files
         files: [{
@@ -25,17 +35,41 @@ module.exports = function(grunt) {
         },
         src: 'dist/css/**/*.css' // can leave the dest off
       }
+    },
+    cssmin: {
+      theme: {
+        options: {
+          banner: '<%= banner %>',
+        },
+
+        expand: true,
+        cwd: 'dist/css/',
+        src: [
+          '**/*.css',
+          '!**/*.min.css'
+        ],
+        ext: '.min.css',
+        dest: 'dist/css/'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   grunt.registerTask(
     'default',
-    'Compile SASS & Vendor Prefix', [
+    'build css', [
+      'buildcss'
+    ]
+  );
+  grunt.registerTask(
+    'buildcss',
+    'Compile SASS, Vendor Prefix minifi it', [
       'sass:theme',
-      'autoprefixer:theme'
+      'autoprefixer:theme',
+      'cssmin:theme'
     ]
   );
 };
