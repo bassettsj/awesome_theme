ca53916268a4519e3f3617d19f5750c2b8bc0f92
Steven Bassett <steven.j.bassett@gmail.com>
2014-06-02 13:08:38 -0500

    Grunt task for autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,11 @@
-module.exports = function(grunt){
+module.exports = function(grunt) {
   grunt.initConfig({
     package: grunt.file.readJSON('package.json'),
     // task
     sass: {
       // target
-      theme:{
-        options:{
+      theme: {
+        options: {
           // note* requires SASS >=3.3.0 `gem install sass --pre`
           sourcemap: true
         },
@@ -13,18 +13,30 @@ module.exports = function(grunt){
         files: [{
           dest: 'dist/css/awesome-theme.css',
           src: 'scss/main.scss'
-        },{
-          
+        }, {
+
         }]
+      }
+    },
+    autoprefixer: {
+      theme: {
+        options: {
+          map: true
+        },
+        src: 'dist/css/**/*.css' // can leave the dest off
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
 
   grunt.registerTask(
     'default',
-    'Compile SASS',
-    ['sass:theme']
+    'Compile SASS & Vendor Prefix',
+    [
+      'sass:theme',
+      'autoprefixer:theme'
+    ]
   );
 };
